6a0c6e6e5291d384a62c27ca351f1abf06a974b2
caiji <caiji202111@gmail>
2022-03-11 00:56:30 +0800

    modify pre and next page

--- pagination.js
+++ pagination.js
@@ -3,14 +3,14 @@ export default{
     template:`<nav aria-label="Page navigation example">
     <ul class="pagination">
       <li class="page-item" :class="{disabled:!pages.has_pre}">
-        <a class="page-link" href="#" aria-label="Previous">
+        <a class="page-link" href="#" aria-label="Previous" @click="$emit('get-product',pages.current_page - 1)">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
       <li class="page-item" :class="{active:page===pages.current_page}"
       v-for="page in pages.total_pages" @click="$emit('get-product',page)" ><a class="page-link" href="#">{{ page }}</a></li>
       <li class="page-item" :class="{disabled:!pages.has_next}">
-        <a class="page-link" href="#" aria-label="Next">
+        <a class="page-link" href="#" aria-label="Next" @click="$emit('get-product',pages.current_page + 1)">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
